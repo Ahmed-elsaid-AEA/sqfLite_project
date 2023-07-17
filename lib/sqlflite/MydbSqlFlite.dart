c298c3762a97b8609a22a9a45a20a3f0a9f918c1
ahmed-abmouty <ahmed122727727@gmail.com>
2023-07-17 18:49:47 +0300

    CREATE DATBASE AND MAKE FIRST TABLE NOTE

--- lib/sqlflite/MydbSqlFlite.dart
+++ lib/sqlflite/MydbSqlFlite.dart
@@ -1,7 +1,28 @@
-class MydbSqlFlite{
+import 'dart:async';
 
-  intialDb() async{
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class MydbSqlFlite {
+  intialDb() async {
     //first put your db in path
-    // String dataBaseBath=get
+    String dataBaseBath =
+        await getDatabasesPath(); //the default datbase location
+    //database Name
+    String path = join(dataBaseBath, 'dataBaseName.db');
+    //or
+    String path2 = dataBaseBath + "/" + "dataBaseName.db";
+    // create dataBase
+    Database myDb = await openDatabase(path, onCreate: _onCreate);
+  }
+
+  FutureOr<void> _onCreate(Database db, int version) async {
+    //make tables
+    await db.execute('''
+    CREATE TABLE "NOTE"(
+   ID INTEGER AUTOINCREMENT NOT NULL PRIMARY KEY ,
+   NOTES TEXT NOT NULL
+    )
+    ''');
   }
 }
